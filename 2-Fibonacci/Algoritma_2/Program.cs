e4fc0c60c01e339d8f98c83bafbc4f3b8869c588
burakibrahim@gmail.com <burakibrahim@gmail.com>
2020-12-19 00:56:38 +0300

    Addition of Fibonacci Series

---
+++ 2-Fibonacci/Algoritma_2/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algoritma_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Sum of equal-valued terms of fibronacci series up to 4 million?????
+            
+            ulong a = 1, b = 2, sum = 0, counter = 2;
+            sum = a + b;
+            while (sum < 4000000)
+            {
+                if (sum % 2 == 0)
+                {
+                    counter += sum;
+                }
+                a = b;
+                b = sum;
+                sum = a + b;
+            }
+            Console.WriteLine(counter);
+            Console.ReadKey();
+        }
+    }
+}
