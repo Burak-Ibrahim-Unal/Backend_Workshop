2a7407968909313398d9d8e988acae1cec56ce7a
Burak Ibrahim Unal <burakibrahim@gmail.com>
2021-08-29 18:00:34 +0300

    New Entities Added
    New Entities Added

---
+++ NetCore OOP Samples/OOP_Test8/Entities/Concrete/Category.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Entities.Concrete
+{
+    public class Category
+    {
+        public int CategoryId { get; set; }
+        public string CategoryName { get; set; }
+
+
+    }
+}
