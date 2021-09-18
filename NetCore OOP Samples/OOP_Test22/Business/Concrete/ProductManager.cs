70f93e75107b1489c581bdbec960da16aab16276
Burak Ibrahim Unal <burakibrahim@gmail.com>
2021-09-18 16:47:11 +0300

    Test 22 Asp.Net Web Api Started
    Test 22 Asp.Net Web Api Started

---
+++ NetCore OOP Samples/OOP_Test22/Business/Concrete/ProductManager.cs
@@ -0,0 +1,68 @@
+﻿using Business.Abstract;
+using Business.Constants;
+using Core.Utilities.Results;
+using DataAccess.Abstract;
+using Entity.Concrete;
+using Entity.Dto;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Business.Concrete
+{
+    public class ProductManager : IProductService
+    {
+
+        IProductDal _productDal;
+
+        public ProductManager(IProductDal productDal)
+        {
+            _productDal = productDal;
+        }
+
+        public IResult Add(Product p)
+        {
+            //Add adding rules here...Business layer...
+            if (p.ProductName.Length < 2)
+            {
+                return new ErrorResult(Messages.ProductNameInvalid);
+            }
+            _productDal.Add(p);
+            return new Result(true, Messages.ProductAdded);
+        }
+
+        public IDataResult<List<Product>> GetAllByCategoryId(int categoryId)
+        {
+            return new SuccessDataResult<List<Product>>(_productDal.GetAll(p => p.CategoryId == categoryId));
+        }
+        public IDataResult<Product> GetById(int productId)
+        {
+            return new SuccessDataResult<Product>(_productDal.Get(p => p.ProductId == productId));
+        }
+
+        public IDataResult<List<Product>> GetAllByUnitPrice(decimal minUnitPrice, decimal maxUnitPrice)
+        {
+            return new SuccessDataResult<List<Product>>(_productDal.GetAll(p => p.UnitPrice >= minUnitPrice && p.UnitPrice <= maxUnitPrice));
+        }
+
+        public IDataResult<List<Product>> GetAllProducts()
+        {
+            if (DateTime.Now.Hour == 15)
+            {
+                return new ErrorDataResult<List<Product>>(Messages.MaintanceTime);
+            }
+            return new SuccessDataResult<List<Product>>(_productDal.GetAll(), Messages.ProductsListed);
+        }
+
+
+
+        public IDataResult<List<ProductDetailDto>> GetProductDetails()
+        {
+            if (DateTime.Now.Hour == 16)
+            {
+                return new ErrorDataResult<List<ProductDetailDto>>(Messages.MaintanceTime);
+            }
+            return new SuccessDataResult<List<ProductDetailDto>>(_productDal.GetProductDetails());
+        }
+    }
+}
---
+++ NetCore OOP Samples/OOP_Test22/DataAccess/Concrete/EntityFramework/NorthwindContext.cs
@@ -0,0 +1,22 @@
+﻿using Entity.Concrete;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataAccess.Concrete.EntityFramework
+{
+    public class NorthwindContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Server=DESKTOP-QUKSOAD;Database=Northwind;Trusted_Connection=true");
+        }
+
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Customer> Customers { get; set; }
+        public DbSet<Order> Orders { get; set; }
+        public DbSet<Employee> Employees { get; set; }
+    }
+}
