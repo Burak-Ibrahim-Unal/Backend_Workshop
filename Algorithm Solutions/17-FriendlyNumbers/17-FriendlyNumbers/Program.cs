31f9a1aa8a48af66db62f56be3f1bcfc1cf0ab6b
Burak Ibrahim Unal <burakibrahim@gmail.com>
2021-11-07 11:53:41 +0300

    Updated program.cs and description added
    Updated program.cs and description added

--- Algorithm Solutions/17-FriendlyNumbers/17-FriendlyNumbers/Program.cs
+++ Algorithm Solutions/17-FriendlyNumbers/17-FriendlyNumbers/Program.cs
@@ -4,6 +4,15 @@ namespace _17_FriendlyNumbers
 {
     class Program
     {
+
+        // Amicable Number Algorithm 
+        // https://en.wikipedia.org/wiki/Amicable_numbers
+        /*
+        The smallest pair of amicable numbers is (220, 284). They are amicable because the proper divisors of 220 are 1, 2, 4, 5, 10, 11, 20, 22, 44, 55 and 110,
+        of which the sum is 284; and the proper divisors of 284 are 1, 2, 4, 71 and 142, 
+        of which the sum is 220.
+        */
+
         static void Main(string[] args)
         {
             calculateAmicableNumber(220, 284);
