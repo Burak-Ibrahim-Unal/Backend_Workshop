b27be80aa4319e486e997e3476c003d1cd95e89e
Burak Ibrahim Unal <burakibrahim@gmail.com>
2021-11-08 12:51:15 +0300

    Data class created
    The data structure by which the server and the client interact with each other

---
+++ .Net Asynchronous Socket Programming/ChatService/Data.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ChatService
+{
+    class Data
+    {
+        public string strName { get; set; } //Name by which the client logs into the room
+        public Command cmdCommand { get; set; } //Message text
+
+        public string strMessage { get; set; } //Command type (login, logout, send message ...)
+
+        public Data()
+        {
+            this.cmdCommand = Command.Null;
+            this.strMessage = null;
+            this.strName = null;
+        }
+    }
+}
