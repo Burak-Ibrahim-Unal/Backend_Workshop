72334166af890382f3aec7cce6288f5abfa05226
Burak Ibrahim Unal <burakibrahim@gmail.com>
2021-11-08 12:44:25 +0300

    Trivial commands added
    Trivial commands added

---
+++ .Net Asynchronous Socket Programming/ChatService/Command.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ChatService
+{
+    enum Command
+    {
+        //Log into the server
+        Login,
+        //Logout of the server
+        Logout,
+        //Send a text message to all the chat clients     
+        Message,
+        //Get a list of users in the chat room from the server
+        List
+    }
+}
