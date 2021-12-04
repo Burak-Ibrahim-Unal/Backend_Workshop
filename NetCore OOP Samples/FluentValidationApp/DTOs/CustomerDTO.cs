6fd16de5659b35a23289fa92aff75f5e580d3a26
Burak Ibrahim Unal <burakibrahim@gmail.com>
2021-12-04 18:05:34 +0300

    New properties added customer-dto // flatten is made// credit card model is added

--- NetCore OOP Samples/FluentValidationApp/DTOs/CustomerDTO.cs
+++ NetCore OOP Samples/FluentValidationApp/DTOs/CustomerDTO.cs
@@ -3,8 +3,14 @@
     public class CustomerDTO
     {
         public int Id { get; set; }
-        public string Name { get; set; }
-        public string Mail { get; set; }
-        public int Age { get; set; } 
+        public string FullName { get; set; }
+        public string Email { get; set; }
+        public int Age { get; set; }
+        public string FullProperty { get; set; }
+        public string CreditCardNumber { get; set; }  // class name + property = automapper standart
+        public string CCNumber { get; set; }  // custom name = not automapper standart,it must me add into customerprofile formember
+        public DateTime CreditCardValidDate{ get; set; } // class name + property = automapper standart
+        public DateTime CCValidDate { get; set; }  // custom name = not automapper standart,it must me add into customerprofile formember
+
     }
 }
