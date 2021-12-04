b04064ffe3a3cfb02df997a4d5018940599de51a
Burak Ibrahim Unal <burakibrahim@gmail.com>
2021-12-04 15:56:47 +0300

    Automapper is added to Net Core 6

---
+++ NetCore OOP Samples/FluentValidationApp/DTOs/CustomerDTO.cs
@@ -0,0 +1,10 @@
+﻿namespace FluentValidationApp.DTOs
+{
+    public class CustomerDTO
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Mail { get; set; }
+        public int Age { get; set; } 
+    }
+}
