4d8a7868df322d2cac3079b565202ab5902fa81c
Burak Ibrahim Unal <burakibrahim@gmail.com>
2021-12-04 14:50:24 +0300

    Update CustomerValidator.cs

--- NetCore OOP Samples/FluentValidationApp/FluentValidators/CustomerValidator.cs
+++ NetCore OOP Samples/FluentValidationApp/FluentValidators/CustomerValidator.cs
@@ -8,12 +8,13 @@ namespace FluentValidationApp.FluentValidators
 
         public string NotEmptyMessage { get; } = "{PropertyName} can not be null or empty";
         public string GenderMessage { get; } = "{PropertyName} 1 is male,2 is female";
+        public string AgeMessage { get; } = "{PropertyName} must be between {PropertyValue} and {PropertyValue}";
 
         public CustomerValidator()
         {
             RuleFor(property => property.Name).NotEmpty().WithMessage("Name can not be empty...");
             RuleFor(property => property.Mail).NotEmpty().WithMessage("Email can not be empty...").EmailAddress().WithMessage("Email must be in correct format...");
-            RuleFor(property => property.Age).NotEmpty().WithMessage("Age can not be empty...").InclusiveBetween(18, 150).WithMessage("Age must be between 18 and 150");
+            RuleFor(property => property.Age).NotEmpty().WithMessage("Age can not be empty...").InclusiveBetween(18, 120).WithMessage(AgeMessage);
             RuleFor(property => property.Birthday).NotEmpty().WithMessage(NotEmptyMessage).Must(property =>
             {
                 return DateTime.Now.AddYears(-18) >= property;
