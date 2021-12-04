b7be51e670b010502c2862594ff91c9b4de5a9cf
Burak Ibrahim Unal <burakibrahim@gmail.com>
2021-12-04 20:09:42 +0300

    Additional features added

--- NetCore OOP Samples/FluentValidationApp/DTOs/CustomerDTO.cs
+++ NetCore OOP Samples/FluentValidationApp/DTOs/CustomerDTO.cs
@@ -8,9 +8,9 @@
         public int Age { get; set; }
         public string FullProperty { get; set; }
         public string CreditCardNumber { get; set; }  // class name + property = automapper standart
-        public string CCNumber { get; set; }  // custom name = not automapper standart,it must me add into customerprofile formember
+        //public string CCNumber { get; set; }  // custom name = not automapper standart,it must me add into customerprofile formember
         public DateTime CreditCardValidDate{ get; set; } // class name + property = automapper standart
-        public DateTime CCValidDate { get; set; }  // custom name = not automapper standart,it must me add into customerprofile formember
+        //public DateTime CCValidDate { get; set; }  // custom name = not automapper standart,it must me add into customerprofile formember
 
     }
 }
