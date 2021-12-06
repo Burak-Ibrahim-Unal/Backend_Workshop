87cf5d128efa7e19a42da0c75c1489b9635b7b3a
Burak Ibrahim Unal <burakibrahim@gmail.com>
2021-12-06 21:34:12 +0300

    Async Demo Windows Form Project Created

---
+++ NetCore OOP Samples/AsenkronApp1/Form1.Designer.cs
@@ -0,0 +1,97 @@
+﻿namespace AsenkronApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnReadTxt = new System.Windows.Forms.Button();
+            this.richTextBoxTxt = new System.Windows.Forms.RichTextBox();
+            this.btnCounter = new System.Windows.Forms.Button();
+            this.textBoxCounter = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btnReadTxt
+            // 
+            this.btnReadTxt.Location = new System.Drawing.Point(38, 12);
+            this.btnReadTxt.Name = "btnReadTxt";
+            this.btnReadTxt.Size = new System.Drawing.Size(91, 43);
+            this.btnReadTxt.TabIndex = 0;
+            this.btnReadTxt.Text = "Read";
+            this.btnReadTxt.UseVisualStyleBackColor = true;
+            this.btnReadTxt.Click += new System.EventHandler(this.btnReadTxt_Click);
+            // 
+            // richTextBoxTxt
+            // 
+            this.richTextBoxTxt.Location = new System.Drawing.Point(12, 70);
+            this.richTextBoxTxt.Name = "richTextBoxTxt";
+            this.richTextBoxTxt.Size = new System.Drawing.Size(154, 106);
+            this.richTextBoxTxt.TabIndex = 1;
+            this.richTextBoxTxt.Text = "";
+            // 
+            // btnCounter
+            // 
+            this.btnCounter.Location = new System.Drawing.Point(223, 21);
+            this.btnCounter.Name = "btnCounter";
+            this.btnCounter.Size = new System.Drawing.Size(91, 43);
+            this.btnCounter.TabIndex = 0;
+            this.btnCounter.Text = "Counter";
+            this.btnCounter.UseVisualStyleBackColor = true;
+            this.btnCounter.Click += new System.EventHandler(this.btnCounter_Click);
+            // 
+            // textBoxCounter
+            // 
+            this.textBoxCounter.Location = new System.Drawing.Point(223, 105);
+            this.textBoxCounter.Name = "textBoxCounter";
+            this.textBoxCounter.Size = new System.Drawing.Size(91, 22);
+            this.textBoxCounter.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(373, 226);
+            this.Controls.Add(this.textBoxCounter);
+            this.Controls.Add(this.richTextBoxTxt);
+            this.Controls.Add(this.btnCounter);
+            this.Controls.Add(this.btnReadTxt);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnReadTxt;
+        private System.Windows.Forms.RichTextBox richTextBoxTxt;
+        private System.Windows.Forms.Button btnCounter;
+        private System.Windows.Forms.TextBox textBoxCounter;
+    }
+}
+
---
+++ NetCore OOP Samples/AsenkronApp1/Form1.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AsenkronApp1
+{
+    public partial class Form1 : Form
+    {
+        public int Counter { get; set; } = 0;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private async void btnReadTxt_Click(object sender, EventArgs e)
+        {
+            //string data = ReadTxtFile(); // to use it remove async keyword and await line
+            string data = await ReadTxtFileAsync();
+            richTextBoxTxt.Text = data;
+        }
+
+        private void btnCounter_Click(object sender, EventArgs e)
+        {
+            textBoxCounter.Text = Counter++.ToString();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+
+        private string ReadTxtFile()
+        {
+            string data = string.Empty;
+            using (StreamReader streamReader = new StreamReader("ReadMeToCs.txt"))
+            {
+                Thread.Sleep(4000); // this is synchronous and we cant use our software 4 seconds...
+                data = streamReader.ReadToEnd();
+            }
+            return data;
+
+        }
+
+
+        // synchronous void = asynchronous Task
+
+
+        private async Task<string> ReadTxtFileAsync()
+        {
+            string data = string.Empty;
+            using (StreamReader streamReader = new StreamReader("ReadMeToCs.txt"))
+            {
+                Task<string> newData = streamReader.ReadToEndAsync();
+
+                await Task.Delay(5000);
+
+                data = await newData;
+
+                return data;
+            }
+        }
+
+
+    }
+
+
+}
