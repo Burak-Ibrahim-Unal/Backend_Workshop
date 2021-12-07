34dd00af338b1d9de31a32c46fd9519732450f5b
Burak Ibrahim Unal <burakibrahim@gmail.com>
2021-12-07 23:04:09 +0300

    Update Program.cs

--- NetCore OOP Samples/AsenkronConsoleApp/Program.cs
+++ NetCore OOP Samples/AsenkronConsoleApp/Program.cs
@@ -2,7 +2,7 @@
 
 var task = new HttpClient().GetStringAsync("https://www.google.com").ContinueWith(data =>
 {
-    Console.WriteLine($"Data length: {data.Result.Length}");
+    Console.WriteLine($"Data length: {data.Result.Length}"); // or we can use any other method...
 });
 
 Console.WriteLine("We have work to do here...");
