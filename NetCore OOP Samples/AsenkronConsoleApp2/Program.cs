9e0c142b241855bad33221f9e394ef0b3642e0bc
Burak Ibrahim Unal <burakibrahim@gmail.com>
2021-12-08 11:50:10 +0300

    New Async console app started

---
+++ NetCore OOP Samples/AsenkronConsoleApp2/Program.cs
@@ -0,0 +1,31 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+List<string> urlList = new List<string>()
+{
+    "https://www.google.com",
+    "https://www.guru3d.com",
+    "https://www.apple.com/tr",
+    "https://www.w3schools.com"
+};
+
+
+
+async Task<PageContent> getContentAsync(string url)
+{
+    PageContent pageContent = new PageContent();
+    var data = await new HttpClient().GetStringAsync(url);
+
+    pageContent.Site = url;
+    pageContent.Len = data.Length;
+    Console.WriteLine($"getContentAsync method works with Thread:{Thread.CurrentThread.ManagedThreadId}");
+    return pageContent;
+
+}
+
+
+class PageContent
+{
+    public string Site { get; set; }
+    public int Len { get; set; }
+}
