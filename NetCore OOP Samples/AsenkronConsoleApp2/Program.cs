419dca9d5fa3055ca582f0ae1bfee637c6ba6f6c
Burak Ibrahim Unal <burakibrahim@gmail.com>
2021-12-09 09:55:44 +0300

    WhenAll is added

--- NetCore OOP Samples/AsenkronConsoleApp2/Program.cs
+++ NetCore OOP Samples/AsenkronConsoleApp2/Program.cs
@@ -1,6 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-
+Console.WriteLine($"Main thread is:{Thread.CurrentThread.ManagedThreadId}");
 List<string> urlList = new List<string>()
 {
     "https://www.google.com",
@@ -11,10 +11,32 @@ List<string> urlList = new List<string>()
 
 
 
+List<Task<PageContent>> PagesList = new List<Task<PageContent>>();
+foreach (var url in urlList)
+{
+    PagesList.Add(getContentAsync(url));
+
+}
+
+
+var results = await Task.WhenAll(PagesList.ToArray());
+
+foreach (var result in results)
+{
+    Console.WriteLine($"{result.Site}, size:{result.Len}");
+}
+
+
+
 async Task<PageContent> getContentAsync(string url)
 {
     PageContent pageContent = new PageContent();
-    var data = await new HttpClient().GetStringAsync(url);
+    var handler = new HttpClientHandler()
+    {
+        AllowAutoRedirect = false
+    };
+    HttpClient client = new HttpClient(handler);
+    var data = await client.GetStringAsync(url);
 
     pageContent.Site = url;
     pageContent.Len = data.Length;
