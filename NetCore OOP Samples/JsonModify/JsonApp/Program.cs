ca5055a78e9b43707e8950b27ba2fd27f81ebf9d
Burak Ibrahim Unal <burakibrahim@gmail.com>
2022-07-13 14:16:54 +0300

    TEST

---
+++ NetCore OOP Samples/JsonModify/JsonApp/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Newtonsoft;
+using Newtonsoft.Json;
+
+
+static void ReadFromJson(string jsonFile)
+{
+    dynamic jsonfile = JsonConvert.DeserializeObject(File.ReadAllText(jsonFile));
+    //Console.WriteLine($"informations:{jsonfile} ");
+    Console.WriteLine(Directory.GetCurrentDirectory());
+
+}
