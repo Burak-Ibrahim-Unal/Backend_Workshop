8410a58cc265cf4166408aa9e478e142a2d154c3
Burak Ibrahim Unal <burakibrahim@gmail.com>
2022-07-13 14:21:11 +0300

    Update Program.cs

--- NetCore OOP Samples/JsonModify/JsonApp/Program.cs
+++ NetCore OOP Samples/JsonModify/JsonApp/Program.cs
@@ -6,7 +6,10 @@ using Newtonsoft.Json;
 static void ReadFromJson(string jsonFile)
 {
     dynamic jsonfile = JsonConvert.DeserializeObject(File.ReadAllText(jsonFile));
-    //Console.WriteLine($"informations:{jsonfile} ");
-    Console.WriteLine(Directory.GetCurrentDirectory());
+    Console.WriteLine($"informations:{jsonfile} ");
+    //Console.WriteLine(Directory.GetCurrentDirectory());
 
 }
+
+string path = @"C:\Users\Burak\Desktop\Github\Backend_Workshop\NetCore OOP Samples\JsonModify\JsonApp\warehouses.json";
+ReadFromJson(path);
