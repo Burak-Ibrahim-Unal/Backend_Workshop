8e50ef55a7aba88234976a23e1c930a3b1173563
Burak Ibrahim Unal <burakibrahim@gmail.com>
2022-07-16 12:06:41 +0300

    JsonTest

--- NetCore OOP Samples/JsonModify/JsonApp/Program.cs
+++ NetCore OOP Samples/JsonModify/JsonApp/Program.cs
@@ -3,13 +3,49 @@ using Newtonsoft;
 using Newtonsoft.Json;
 
 
-static void ReadFromJson(string jsonFile)
+static string ReadFromJson(string jsonFile)
 {
-    dynamic jsonfile = JsonConvert.DeserializeObject(File.ReadAllText(jsonFile));
-    Console.WriteLine($"informations:{jsonfile} ");
+    dynamic jsonResult = JsonConvert.DeserializeObject(File.ReadAllText(jsonFile));
+    Console.WriteLine(jsonResult);
+    return jsonResult.ToString();
     //Console.WriteLine(Directory.GetCurrentDirectory());
 
 }
 
 string path = @"C:\Users\Burak\Desktop\Github\Backend_Workshop\NetCore OOP Samples\JsonModify\JsonApp\warehouses.json";
-ReadFromJson(path);
+var result = ReadFromJson(path);
+
+
+
+public class Stock
+{
+    public string Name { get; set; }
+    public string Id { get; set; }
+    public Location Location { get; set; }
+    public List<Car> Cars { get; set; }
+}
+
+
+public class Location
+{
+    public string lat { get; set; }
+    public string lon { get; set; }
+}
+
+public class Car
+{
+    public string Location { get; set; }
+    public List<Vehicle> Vehicles { get; set; }
+}
+
+public class Vehicle
+{
+    public string Id { get; set; }
+    public string Make { get; set; }
+    public string Model { get; set; }
+    public string Year_Model { get; set; }
+    public decimal Price { get; set; }
+    public bool Licenced { get; set; }
+    public DateTime Date_Added { get; set; }
+
+}
